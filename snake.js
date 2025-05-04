0f2e038386cc1562c9dbbb9bd6cd047a473037ba
NickMacJetpack <nick@nickmacdesign.com>
2025-05-04 11:07:31 +1000

    Update snake.js

--- snake.js
+++ snake.js
@@ -56,4 +56,163 @@ game = {
 };
 
 snake = {
-  size
+  size: canvas.width / 40,
+  x: null,
+  y: null,
+  color: '#0F0',
+  direction: 'left',
+  sections: [],
+
+  init: function () {
+    snake.sections = [];
+    snake.direction = 'left';
+    snake.x = canvas.width / 2 + snake.size / 2;
+    snake.y = canvas.height / 2 + snake.size / 2;
+    for (i = snake.x + 5 * snake.size; i >= snake.x; i -= snake.size) {
+      snake.sections.push(i + ',' + snake.y);
+    }
+  },
+
+  move: function () {
+    switch (snake.direction) {
+      case 'up': snake.y -= snake.size; break;
+      case 'down': snake.y += snake.size; break;
+      case 'left': snake.x -= snake.size; break;
+      case 'right': snake.x += snake.size; break;
+    }
+    snake.checkCollision();
+    snake.checkGrowth();
+    snake.sections.push(snake.x + ',' + snake.y);
+  },
+
+  draw: function () {
+    for (i = 0; i < snake.sections.length; i++) {
+      snake.drawSection(snake.sections[i].split(','));
+    }
+  },
+
+  drawSection: function (section) {
+    game.drawBox(parseInt(section[0]), parseInt(section[1]), snake.size, snake.color);
+  },
+
+  checkCollision: function () {
+    if (snake.isCollision(snake.x, snake.y)) {
+      game.stop();
+    }
+  },
+
+  isCollision: function (x, y) {
+    return (
+      x < snake.size / 2 ||
+      x > canvas.width ||
+      y < snake.size / 2 ||
+      y > canvas.height ||
+      snake.sections.indexOf(x + ',' + y) >= 0
+    );
+  },
+
+  checkGrowth: function () {
+    if (snake.x === food.x && snake.y === food.y) {
+      game.score++;
+      if (game.score % 5 === 0 && game.fps < 60) {
+        game.fps++;
+      }
+      food.set();
+    } else {
+      snake.sections.shift();
+    }
+  }
+};
+
+food = {
+  size: null,
+  x: null,
+  y: null,
+  color: '#0f0',
+
+  set: function () {
+    food.size = snake.size;
+    food.x = (Math.ceil(Math.random() * 10) * snake.size * 4) - snake.size / 2;
+    food.y = (Math.ceil(Math.random() * 10) * snake.size * 3) - snake.size / 2;
+  },
+
+  draw: function () {
+    game.drawBox(food.x, food.y, food.size, food.color);
+  }
+};
+
+inverseDirection = {
+  up: 'down',
+  left: 'right',
+  right: 'left',
+  down: 'up'
+};
+
+keys = {
+  up: [38, 75, 87],
+  down: [40, 74, 83],
+  left: [37, 65, 72],
+  right: [39, 68, 76],
+  start_game: [13, 32]
+};
+
+Object.prototype.getKey = function (value) {
+  for (var key in this) {
+    if (this[key] instanceof Array && this[key].indexOf(value) >= 0) {
+      return key;
+    }
+  }
+  return null;
+};
+
+function isTouchDevice() {
+  return "ontouchstart" in document.documentElement;
+}
+
+if (isTouchDevice()) {
+  addEventListener("touchstart", handleEvent, false);
+} else {
+  addEventListener("keydown", function (e) {
+    lastKey = keys.getKey(e.keyCode);
+    if (['up', 'down', 'left', 'right'].includes(lastKey) &&
+        lastKey !== inverseDirection[snake.direction]) {
+      snake.direction = lastKey;
+    } else if (['start_game'].includes(lastKey) && game.over) {
+      game.start();
+    }
+  }, false);
+  addEventListener("click", handleEvent, false);
+}
+
+function handleEvent(e) {
+  e.stopPropagation();
+  if (game.over) {
+    game.start();
+  }
+  if (snake.direction === 'left') snake.direction = 'down';
+  else if (snake.direction === 'down') snake.direction = 'right';
+  else if (snake.direction === 'right') snake.direction = 'up';
+  else if (snake.direction === 'up') snake.direction = 'left';
+}
+
+var requestAnimationFrame = requestAnimationFrame ||
+  webkitRequestAnimationFrame ||
+  mozRequestAnimationFrame ||
+  msRequestAnimationFrame ||
+  oRequestAnimationFrame;
+
+function loop() {
+  if (!game.over) {
+    game.resetCanvas();
+    game.drawScore();
+    snake.move();
+    food.draw();
+    snake.draw();
+    game.drawMessage();
+  }
+  setTimeout(function () {
+    requestAnimationFrame(loop);
+  }, 1000 / game.fps);
+}
+
+requestAnimationFrame(loop);
